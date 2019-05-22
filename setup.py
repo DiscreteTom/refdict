4738d7a6b75d57dcc9fd2306a1527d18951ca72d
DiscreteTom <discrete_tom@outlook.com>
2019-05-22 18:50:03 +0800

    release v1.0.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="refdict",
+    version="1.0.0",
+    author="DiscreteTom",
+    author_email="discrete_tom@outlook.com",
+    description="add reference for dict in python using formatted string",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiscreteTom/RefDict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+				"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)

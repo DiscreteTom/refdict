c205ffccf20f7732568665ed054445a18e931ded
DiscreteTom <discrete_tom@outlook.com>
2019-05-25 23:07:32 +0800

    finish v2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="refdict",
-    version="2.0.1",
+    version="2.1.0",
     author="DiscreteTom",
     author_email="discrete_tom@outlook.com",
     description="add reference for dict in python using formatted string",

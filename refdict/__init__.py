1be1285512f0ffd88c394e7f77a89eceb60accce
DiscreteTom <discrete_tom@outlook.com>
2019-05-31 10:06:15 +0800

    update __getattr__ to adapt partial result

--- refdict/__init__.py
+++ refdict/__init__.py
@@ -120,7 +120,10 @@ class refdict:
 		return result
 
 	def __getattr__(self, funcName):
-		return eval('self._refdict__data.' + funcName)
+		if self._refdict__partial:
+			return eval('self._refdict__result.' + funcName)
+		else:
+			return eval('self._refdict__data.' + funcName)
 
 	def __str__(self):
 		return str(self.__data)

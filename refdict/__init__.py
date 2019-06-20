5494fecabca6c2c733472ee1f72f260318b70522
DiscreteTom <discrete_tom@outlook.com>
2019-06-20 20:45:03 +0800

    add refdict.get(keys, default)

--- refdict/__init__.py
+++ refdict/__init__.py
@@ -190,3 +190,11 @@ class refdict:
 		result.__result = refdict.findItem(result._refdict__data, keys, refPrefix = self.__prefix, separator=self.__separator, root=result._refdict__result)
 		return result
 
+	def get(self, keys, default):
+		'''
+		similiar to `dict.get()`
+		'''
+		if keys in self:
+			return self[keys]
+		else:
+			return default

aeacf7bd0fbdd7253877e083088807d246eb8e83
DiscreteTom <discrete_tom@outlook.com>
2019-07-27 11:18:53 +0800

    release v3.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="refdict",
-    version="3.2.1",
+    version="3.3.0",
     author="DiscreteTom",
     author_email="discrete_tom@outlook.com",
     description="Using string as chain keys to realize chain access and reference access of dict in python",
